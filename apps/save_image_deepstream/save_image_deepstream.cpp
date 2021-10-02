3be5ed84c0ef7a83d7caddc8eaa9508b1103d972
tienln4 <ngoctien.hust@gmail.com>
2021-10-02 09:45:58 +0000

    save image in deepstream

---
+++ apps/save_image_deepstream/save_image_deepstream.cpp
@@ -0,0 +1,9 @@
+#include "save_image_deepstream/save_image_deepstream.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout<<"Hello_world"<<std::endl;
+    save_image_deepstream(argc, argv);
+    return 0;
+}
